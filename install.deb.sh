5e265c713842bf7348e4612353d7738ec69584f2
char46 <haosiru@h-sr.cn>
2025-12-03 11:19:32 +0800

    Modify starship installation script
    Updated installation script to use -y flag and save config.

--- install.deb.sh
+++ install.deb.sh
@@ -1,6 +1,8 @@
 # starship
-curl -sS https://starship.rs/install.sh | sh
+curl -sS https://starship.rs/install.sh | sh -s -- -y
 mkdir -p ~/.config
-wget 'https://raw.githubusercontent.com/char-46/char-46/refs/heads/master/starship.toml' -o ~/.config/
+wget 'https://raw.githubusercontent.com/char-46/char-46/refs/heads/master/starship.toml' -O ~/.config/starship.toml
+echo 'eval "$(starship init zsh)"' >> ~/.zshrc
+echo 'eval "$(starship init bash)"' >> ~/.bashrc
 
 # TODO: fnm conda...
