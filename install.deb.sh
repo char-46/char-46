d7fe2eeb011bd1ebef5ccd70fb79a653382878f1
char46 <haosiru@h-sr.cn>
2025-12-03 11:11:39 +0800

    Add installation script for Starship

---
+++ install.deb.sh
@@ -0,0 +1,6 @@
+# starship
+curl -sS https://starship.rs/install.sh | sh
+mkdir -p ~/.config
+wget 'https://raw.githubusercontent.com/char-46/char-46/refs/heads/master/starship.toml' -o ~/.config/
+
+# TODO: fnm conda...
